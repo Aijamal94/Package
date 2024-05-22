0596eb698f395e81ee1b4d48e5a0fce85c607a56
Aizhamal <aijashka79@gmail.com>
2024-05-22 16:36:45 +0500

    Add VacationServiceTest file

--- src/main/java/ru/netology/services/VacationService.java
+++ src/main/java/ru/netology/services/VacationService.java
@@ -2,15 +2,28 @@ package ru.netology.services;
 
 public class VacationService {
 
-    public int calculate(int income, int expenses) {
-        int count = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
-        for (int month = 0; month < 12; month++) {
-            if (money >= expenses) { // можем ли отдыхать?
-                count++; // увеличиваем счётчик месяцев отдыха
-                money = money - expenses;
-            } else {
-                money = money + income;
+
+        public int calculate(int income, int expenses, int threshold) {
+            int count = 0; // счётчик месяцев отдыха
+            int money = 0; // количество денег на счету
+
+            for (int month = 0; month < 12; month++) {
+                if (money >= threshold) { // можем ли отдыхать?
+                    count++; // увеличиваем счётчик месяцев отдыха
+                    money -= expenses; // уменьшаем остаток на траты на отдых
+                    money /= 3; // уменьшаем остаток еще в три раза
+                } else {
+                    money += income - expenses; // доход за вычетом расходов
+                }
             }
+
+            return count;
         }
-        return count;
+    }
+
+
+
+
+
+
+
--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,19 +1,17 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    public class VacationServiceTest {
+public class VacationServiceTest {
 
-        @Test
-        public void testVacationCount() {
-            VacationService vacationService = new VacationService();
-
-            // Первый сценарий из задания
-            assertEquals(3, vacationService.calculate(10000, 3000, 20000));
-
-            // Второй сценарий из задания
-            assertEquals(2, vacationService.calculate(100000, 60000, 150000));
-        }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test-data.csv", numLinesToSkip = 1)
+    public void testVacationCount(int income, int expenses, int threshold, int expected) {
+        VacationService vacationService = new VacationService();
+        assertEquals(expected, vacationService.calculate(income, expenses, threshold));
     }
+}
+
